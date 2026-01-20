9c8ff74def5bb90bdc071405cd7c58d805f1db52
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2026-01-20 08:23:18 +0100

    Fix(getcommit)

--- get_commit.sh
+++ get_commit.sh
@@ -9,7 +9,8 @@ echo "--- Extraction des commits (avec auteur) vers $OUTPUT_FILE ---"
 # En-tête du CSV
 echo "Hash,Auteur,Jour,Heure,Message" > "$OUTPUT_FILE"
 
-
+# Format Git : 
+# %h : hash | %an : nom de l'auteur | %ad : date formatée | %s : sujet
 git log --since="$SINCE" --date=format:'%A %H:%M' --pretty=format:"%h|%an|%ad|%s" | \
 awk -F'|' '{
     split($3, date_parts, " ");
@@ -17,16 +18,17 @@ awk -F'|' '{
     hour_min = date_parts[2];
     hour = substr(hour_min, 1, 2);
 
-
+    # Filtrage : Mardi et avant 12h
     if ((day ~ /[Tt]uesday/ || day ~ /[Mm]ardi/) && hour < 12) {
         hash = $1;
         author = $2;
         message = $4;
 
-
+        # Sécurité : on nettoie les virgules pour ne pas casser les colonnes CSV
         gsub(/,/, " ", author);
         gsub(/,/, " ", message);
 
+        # Ecriture dans le fichier
         print hash "," author "," day "," hour_min "," message
     }
 }' >> "$OUTPUT_FILE"

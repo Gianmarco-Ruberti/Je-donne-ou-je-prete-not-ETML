3f1efab8bc87ddcf31680d4786039f212c6fdee0
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-03-24 11:07:19 +0100

    feat( création ) : on peux crée des objets a chercher
    [37][DONE]

--- start/routes.ts
+++ start/routes.ts
@@ -23,32 +23,29 @@ router.group(() => {
 
   // DonationObjects routes
   router.get('/home', [DonationObjectsController, 'index']).as('donation_objects.index')
-  router.get('/ew', [DonationObjectsController, 'create']).as('donation_objects.create')
+  router.get('/new', [DonationObjectsController, 'create']).as('donation_objects.create')
+  router.post('/', [DonationObjectsController, 'store']).as('donation_objects.store')
 
   router.get('/cherche/home', [ChercheObjectsController, 'index']).as('cherche_objects.index')
+  router.get('/cherche/new', [ChercheObjectsController, 'create']).as('cherche_objects.create')
+  router.post('/cherche', [ChercheObjectsController, 'store']).as('cherche_objects.store')
 
   router.group(() => {
     router.get('/:id', [DonationObjectsController, 'show']).as('donation_objects.show')
     router.get('/:id/edit', [DonationObjectsController, 'edit']).as('donation_objects.edit')
-
-    router.post('/', [DonationObjectsController, 'store']).as('donation_objects.store')
     router.post('/:id', [DonationObjectsController, 'update']).as('donation_objects.update')
     router.delete('/:id', [DonationObjectsController, 'destroy']).as('donation_objects.destroy')
-
     router.post('/:id/reserve', [DonationObjectsController, 'reserve']).as('donation_objects.reserve')
     router.patch('/:id/republish', [DonationObjectsController, 'republish']).as('item.republish')
-
-    router.group(() => {
-      router.get('/:id', [ChercheObjectsController, 'show']).as('cherche_objects.show')
-      router.get('/:id/edit', [ChercheObjectsController, 'edit']).as('cherche_objects.edit')
-
-      router.post('/', [ChercheObjectsController, 'store']).as('cherche_objects.store')
-      router.post('/:id', [ChercheObjectsController, 'update']).as('cherche_objects.update')
-      router.delete('/:id', [ChercheObjectsController, 'destroy']).as('cherche_objects.destroy')
-
-      router.post('/:id/reserve', [ChercheObjectsController, 'reserve']).as('cherche_objects.reserve')
-      router.patch('/:id/republish', [ChercheObjectsController, 'republish']).as('cherche_item.republish')
-    }).prefix('/cherche')
   }).prefix('/item')
+
+  router.group(() => {
+    router.get('/:id', [ChercheObjectsController, 'show']).as('cherche_objects.show')
+    router.get('/:id/edit', [ChercheObjectsController, 'edit']).as('cherche_objects.edit')
+    router.post('/:id', [ChercheObjectsController, 'update']).as('cherche_objects.update')
+    router.delete('/:id', [ChercheObjectsController, 'destroy']).as('cherche_objects.destroy')
+    router.post('/:id/reserve', [ChercheObjectsController, 'reserve']).as('cherche_objects.reserve')
+    router.patch('/:id/republish', [ChercheObjectsController, 'republish']).as('cherche_item.republish')
+  }).prefix('/item/cherche')
 
 }).use(middleware.auth())
